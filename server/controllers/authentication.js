0a526c6b81cf5c098e1813c2b6a1962adce027b5
Rommel Rico <me@rommelrico.com>
2019-05-26 21:12:14 -0700

    fix(index): URL parsing in mongo connect is deprecated

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,7 @@
 exports.signup = function(req, res, next) {
-  res.send({ success: true });
+  console.log(req.body);
+  // See if a user with the given email exists.
+  // If a user with email does exist, return an error
+  // If a user with email does not exist, create and save a user record
+  // Respond to request indicating the user was created.
 };
