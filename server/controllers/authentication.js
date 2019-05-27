f25336b91b0bc0ae44caee1d7c891151b1488ad4
Rommel Rico <me@rommelrico.com>
2019-05-26 21:13:16 -0700

    feat(authentication): Grabbing email and password from JSON body

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,5 +1,7 @@
 exports.signup = function(req, res, next) {
-  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
   // See if a user with the given email exists.
   // If a user with email does exist, return an error
   // If a user with email does not exist, create and save a user record
