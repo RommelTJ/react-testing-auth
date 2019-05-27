e4ca4b55223417d99740bb0c77a15bfb9848d5b8
Rommel Rico <me@rommelrico.com>
2019-05-26 21:20:41 -0700

    feat(authentication): Creating User Records

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,10 +7,26 @@ exports.signup = function(req, res, next) {
   // See if a user with the given email exists.
 
   User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: "Email is in use." }); // Unprocessable Entity
+    }
+
+    // If a user with email does not exist, create and save a user record
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created.
+      res.json(user);
+    });
   });
 
-  // If a user with email does exist, return an error
-  // If a user with email does not exist, create and save a user record
-  // Respond to request indicating the user was created.
 };
